a3c0d10233d63af5f3eb8387a6700be2620fab5d
VanesaChauca <103292927+VanesaChauca@users.noreply.github.com>
2024-06-18 06:18:11 -0300

    feat: muestro los detalles de pelicula con elementos HTML en detalle-peli.htm

--- js/detalle.js
+++ js/detalle.js
@@ -12,3 +12,54 @@ async function cargarDetallePelicula() {
     }
 }
 
+//detalles que mostraremos a traves elementos HTML - DOM
+function mostrarDetalle(data){
+    const contenedorPelicula = document.createElement('div');
+    contenedorPelicula.classList.add('pelicula-detalles');
+
+    const encabezado = document.createElement('div');
+    encabezado.classList.add('encabezado');
+
+    const titulo = document.createElement('h2');
+    titulo.innerHTML = `${data.Title} - ${data.Type}`;
+
+    encabezado.appendChild(titulo);
+
+    const posterPelicula = document.createElement('div');
+    posterPelicula.classList.add('poster-pelicula');
+
+    const imagenPoster = document.createElement('img');
+    imagenPoster.src = data.Poster;
+    imagenPoster.alt = "Poster de la película";
+
+    posterPelicula.appendChild(imagenPoster);
+
+    const calificacion = document.createElement('p');
+    calificacion.innerHTML = `<i>Calificación: ${data.imdbRating}/10</i>`;
+
+    const listaDetalle = document.createElement('ul');
+    listaDetalle.classList.add('lista-detalle');
+    listaDetalle.innerHTML = `
+        <li>Año: ${data.Year}</li>
+        <li>País: ${data.Country}</li>
+        <li>Actores: ${data.Actors}</li>
+        <li>Director: ${data.Director}</li>
+        <li>Género: ${data.Genre}</li>
+        <li>Idioma: ${data.Language}</li>
+        <li>Duración: ${data.Runtime}</li>
+    `;
+
+    const descripcion = document.createElement('p');
+    descripcion.innerText = data.Plot;
+
+    contenedorPelicula.appendChild(posterPelicula);
+    contenedorPelicula.appendChild(encabezado);
+    contenedorPelicula.appendChild(calificacion);
+    contenedorPelicula.appendChild(listaDetalle);
+    contenedorPelicula.appendChild(descripcion);
+
+    const container = document.querySelector('.container-detalle');
+    container.appendChild(contenedorPelicula);
+}
+
+document.addEventListener('DOMContentLoaded', cargarDetallePelicula);
