9e6b47853358467d2f7c93bef0b32c4458c4105f
VanesaChauca <103292927+VanesaChauca@users.noreply.github.com>
2024-06-17 23:54:58 -0300

    feat: consultar a la url  para obtener peliculas

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//key de API OMDb
+const key = "4c18124e";
+
+const buscarPelicula = document.getElementById("Input");
+const mostrarListaBusqueda = document.getElementsByClassName("container-movies");
+
+//consultar datos por URL
+async function fetchPelicula(id){
+    try{
+        const url = `http://www.omdbapi.com/?i=${id}&apikey=${key}`;
+        const respuesta = await fetch(url)
+        if (!respuesta.ok){
+            throw new Error ("No hubo una conexion realizada"); 
+        }
+        const data = await respuesta.json();
+        return data;
+    }catch (error){
+        console.error ("error en obtener peliculas", error);
+    }
+}
