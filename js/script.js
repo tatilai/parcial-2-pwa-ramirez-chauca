81aeadda5424ebfdac0fd849f1a921c83c78043b
VanesaChauca <103292927+VanesaChauca@users.noreply.github.com>
2024-06-17 23:57:27 -0300

    feat: recorro el array de peliculas y las muestro con html

--- js/script.js
+++ js/script.js
@@ -18,3 +18,38 @@ async function fetchPelicula(id){
         console.error ("error en obtener peliculas", error);
     }
 }
+
+//crear los items peliculas
+async function listaPeliculas(peliculas){
+    try {
+        //recorro el array map y genero el HTML para visualizar la pelicula
+        const elementosPelicula = peliculas.map(pelicula => {
+            const poster = pelicula.Poster !== "N/A" ? pelicula.Poster : "No hay un poster de pelicula";
+            const id = pelicula.imdbID;
+            return`
+            <div class="movie-item"
+                <div class="poster-movie">
+                    <a href="detalle-peli.html?id=${id}">
+                    <img src="${poster}" alt="Poster de la pelicula"></a>
+                </div>
+                <div class="movie-detalles>
+                    <div class"movie">
+                        <div>
+                            <p class="movie-name"><a href="detalle-peli.html?id=${id}">${pelicula.Title}</a></p>
+                            <p class="movie-year"><a href="detalle-peli.html?id=${id}">${pelicula.Year}</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+        })
+        .join("");
+        
+        const container = document.querySelector(".container-movies");
+        container.innerHTML = elementosPelicula;
+        console.log(peliculas);
+
+    }catch (error){
+        console.error("Error en mostrar las peliculas en Inicio", error);
+    }
+}
+
