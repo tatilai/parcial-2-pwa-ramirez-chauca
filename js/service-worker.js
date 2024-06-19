236934de946b8e15c9758efd09d62817198fd34a
tatilai <99092149+tatilai@users.noreply.github.com>
2024-06-18 23:00:52 -0300

    feat:construccion del service worker

---
+++ js/service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME= 'Popcorn-app-cache-v1';
+const urlsToCache=[
+    '/',
+    '/home.html',
+    '/detalle-peli.html',
+    '/style.css',
+    '/script.js',
+    '/detalle.js',
+    '/imagen/icon-64x64.png'
+];
+
