ea9f5d1fd41cf2388cb13dd3dce3e286d8ab4498
tatilai <99092149+tatilai@users.noreply.github.com>
2024-06-22 01:00:24 -0300

    Update historial.js

--- js/historial.js
+++ js/historial.js
@@ -34,37 +34,3 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-
-/*document.addEventListener('DOMContentLoaded', () => {
-    const historial = JSON.parse(localStorage.getItem('historial')) || [];
-    const historialLista = document.getElementById('historialLista');
-
-    if (historial.length > 0) {
-        historial.forEach(id => {
-            fetch(`https://www.omdbapi.com/?i=${id}&apikey=${key}`)
-                .then(response => response.json())
-                .then(data => {
-                    const listaItem = document.createElement('div');
-                    listaItem.classList.add('card');
-                    listaItem.innerHTML = `
-                        <div class="card-image">
-                            <img src="${data.Poster}" alt="Poster de ${data.Title}">
-                            <span class="card-title">${data.Title}</span>
-                        </div>
-                        <div class="card-content">
-                            <p>${data.Year}</p>
-                            <p>${data.Genre}</p>
-                        </div>
-                    `;
-                    historialLista.appendChild(listaItem);
-                })
-                .catch(error => {
-                    console.error('Se produjo un error:', error);
-                });
-        });
-    } else {
-        const listaItem = document.createElement('p');
-        listaItem.textContent = 'No hay elementos en el historial.';
-        historialLista.appendChild(listaItem);
-    }
-});*/
