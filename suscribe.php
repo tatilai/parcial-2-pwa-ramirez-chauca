fa59cb7d191b90cd04e3fe88080a95b4be627323
tatilai <99092149+tatilai@users.noreply.github.com>
2024-06-29 13:45:36 -0300

    feat: proceso de validacion de datos back

---
+++ suscribe.php
@@ -0,0 +1,39 @@
+<?PHP
+//inicializo variables con los datos que necesito traer de la superglobal
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+
+function soloLetras($str){
+    return preg_match('/^([^0-9]*)$/', $str) and preg_match('/^[a-zA-Z0-9]{4,10}$/', $str);
+}
+
+if (!(soloLetras($name))){
+    
+    echo("<div>
+            <h1>Ooops</h1>
+            <p>El campo nombre  no pueden contener números o caracteres especiales.</p>
+        </div>");
+
+}
+
+if ((isset($name) and !empty(trim($name))) and
+    (isset($email) and !empty(trim($email)))
+    
+
+) {
+    echo "<pre>";
+    print_r("<div>
+            <h2>Muchas gracias por tu mensaje!</h2>
+                <p>Te contactaremos a la brevedad.</p>
+    
+                <h3>Estos son los datos que nos enviaste:</h3>
+                <ul>
+                    <li><p>Nombre: $name </p></li>
+                    <li><p>Email: $email </p></li>
+                   
+                </ul>
+         </div>");
+    echo "</pre>";
+    
+}
