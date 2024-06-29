7bc0685b48c80787d3e8b96f21cb88f12a4230bb
tatilai <99092149+tatilai@users.noreply.github.com>
2024-06-29 12:57:21 -0300

    envio de formulario por POST

---
+++ js/form-validacion.js
@@ -0,0 +1,35 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form');
+    const nameInput = document.getElementById('name');
+    const ageInput = document.getElementById('age');
+
+    form.addEventListener('submit', function(event) {
+        // Validación del nombre
+        if (nameInput.value.length < 3) {
+            alert('El nombre debe tener al menos 3 caracteres.');
+            event.preventDefault(); // Evita que se envíe el formulario
+            return;
+        }
+
+        // Validación de la edad
+        const age = parseInt(ageInput.value);
+        if (isNaN(age) || age < 18 || age > 100) {
+            alert('Por favor, ingrese una edad válida entre 18 y 100 años.');
+            event.preventDefault(); // Evita que se envíe el formulario
+            return;
+        }
+
+       
+    });
+});*/
+
+
+/*const nombre = document.getElementById("name")
+const url = document.getElementById("url")
+const age = document.getElementById("age")
+const form=document.getElementById("form")
+const parrafo =document.getElementById("warnings")
+
+form.addEventListener("submit",e=>{
+    if(nombre.)
+})*/
--- js/script.js
+++ js/script.js
@@ -183,6 +183,32 @@ setTimeout( () => {
 }, 200);
 
 
+//validacion del formulario
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form');
+    const nameInput = document.getElementById('name');
+    const ageInput = document.getElementById('age');
+
+    form.addEventListener('submit', function(event) {
+        // Validación del nombre
+        if (nameInput.value.length < 3) {
+            alert('El nombre debe tener al menos 3 caracteres.');
+            event.preventDefault(); // Evita que se envíe el formulario
+            return;
+        }
+
+        // Validación de la edad
+        const age = parseInt(ageInput.value);
+        if (isNaN(age) || age < 18 || age > 100) {
+            alert('Por favor, ingrese una edad válida entre 18 y 100 años.');
+            event.preventDefault(); // Evita que se envíe el formulario
+            return;
+        }
+
+       
+    });
+});
 
 /*if(navigator?.serviceWorker) {
     navigator.serviceWorker.register('./sw.js').then((register) => {
